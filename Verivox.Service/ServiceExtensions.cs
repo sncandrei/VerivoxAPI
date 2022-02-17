b345872c97353b6ea1217245ceba4e14aa09c7ac
sncandrei <sincandrei@gmail.com>
2022-02-17 21:41:39 +0200

    usings

--- Verivox.Service/ServiceExtensions.cs
+++ Verivox.Service/ServiceExtensions.cs
@@ -1,10 +1,4 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Verivox.Services;
 using Verivox.Contracts;
 
 namespace Verivox.Services
--- Verivox.Tests/UnitTests/ConsumptionServiceTests.cs
+++ Verivox.Tests/UnitTests/ConsumptionServiceTests.cs
@@ -1,10 +1,4 @@
-﻿using Moq;
-using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
 using Verivox.Contracts;
 using Verivox.Services;
 using Verivox.Services.Models;
